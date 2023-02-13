8714d1a1c1cf703d5c37fb4526d81fb8d02ffd2f
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-13 05:48:56 -0600

    update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './index.css'
+import { useSpring, animated } from '@react-spring/web'
+
+export default function MyComponent() {
+  const springs = useSpring({
+    from: { x: 0 },
+    to: { x: 100 },
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <animated.div
+          style={{
+            width: 80,
+            height: 80,
+            background: '#ff6d6d',
+            borderRadius: 8,
+            ...springs,
+          }}
+      />
+  )
 }
 
-export default App;
+  
